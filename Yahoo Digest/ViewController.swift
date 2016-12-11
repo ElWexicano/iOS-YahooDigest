a65385383f110fc88915f3169288d733cfcbf6b8
Shane Doyle <shanebryandoyle@gmail.com>
2016-12-11 16:29:40 +0000

    Initial Commit

---
+++ Yahoo Digest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yahoo Digest
+//
+//  Created by Shane Doyle on 11/12/2016.
+//  Copyright © 2016 Shane Doyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
